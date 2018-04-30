e111158d62af1ecfd0d8053538cc581afeda8c83
starcatjazz <jasminekivari@gmail.com>
2018-04-29 23:55:49 -0700

    Add files via upload

---
+++ FizzBuzz/Lab1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "";
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    str += "FizzBuzz,";
+                }
+                
+                if (i == 100)
+                {
+
+                    if (i % 3 == 0)
+                    {
+                        str += "Fizz";
+                    }
+                    if (i % 5 == 0)
+                    {
+                        str += "Buzz";
+                    }
+
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    str += "Fizz,";
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    str += "Buzz,";
+                    continue;
+                }
+                str += i + ",";
+                
+            }
+            Console.WriteLine(str);
+            Console.ReadLine();
+            
+        }
+    }
+}
+
+
+
+
